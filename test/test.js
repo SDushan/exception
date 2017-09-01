7ce62d5b5e1199b0bb1b3e2dbac1d1e60ed26123
Tharkana Kodagoda <tharkana.infor@gmail.com>
2017-09-01 18:07:25 +0530

    test 78

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ describe('Check exception', () => {
     });
 
     it('Should have error properties', () => {
-        let exception = new Exception('Custom Error #SD1234#');
+        let exception = new Exception('Custom Error #SD1223433334#');
         expect(exception.name).to.equals('Exception');
         expect(exception.message).to.equals('Custom Error');
         expect(exception.stack).not.to.be.empty;             
