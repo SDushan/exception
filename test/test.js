d88d207aca043403c477cad617db56a4e6d3330f
Samitha Walpola <samithadushan@gmail.com>
2017-09-01 15:27:13 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ describe('Check exception', () => {
     });
 
     it('Should have error properties', () => {
-        let exception = new Exception('Custom Error #SDtrt#');
+        let exception = new Exception('Custom Error #SDtrtgh#');
         expect(exception.name).to.equals('Exception');
         expect(exception.message).to.equals('Custom Error');
         expect(exception.stack).not.to.be.empty;             
